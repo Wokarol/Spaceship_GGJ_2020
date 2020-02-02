fe32d3ee65c75a6832823024780f20db99b11ca9
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-02-02 08:31:33 +0100

    08:31

---
+++ Spaceship_GGJ_2020/Assets/Game/Scripts/Runtime/EnemyMovementPath/ModuleNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleNode : MovementNode
+{
+    [SerializeField] private ShipModule module;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (module == null)
+            return;
+        Gizmos.DrawLine(transform.position, module.transform.position);
+    }
+}
---
+++ Spaceship_GGJ_2020/Assets/Game/Scripts/Runtime/EnemyMovementPath/MovementNode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovementNode : MonoBehaviour
+{
+    [SerializeField] private MovementNode[] transitions = new MovementNode[0];
+
+    private void OnDrawGizmos()
+    {
+        foreach (var node in transitions)
+        {
+            if (node == null)
+                continue;
+
+            Gizmos.color = Color.Lerp(Color.gray, Color.white, 0.5f);
+            Gizmos.DrawLine(transform.position, (node.transform.position + transform.position) / 2);
+        }
+    }
+}
