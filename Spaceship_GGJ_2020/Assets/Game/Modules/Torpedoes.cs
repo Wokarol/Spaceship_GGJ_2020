250450358eed81a0d61e735ec15701d8909d7a41
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2020-02-02 09:07:10 +0100

    09:07

--- Spaceship_GGJ_2020/Assets/Game/Modules/Torpedoes.cs
+++ Spaceship_GGJ_2020/Assets/Game/Modules/Torpedoes.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using NaughtyAttributes;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Torpedoes : ShipModule
 {
+    [SerializeField] private TorpedoLoader loader;
+
+    [Button("Load")]
+    public void Load()
+    {
+        loader.LoadTorpedo(() => { });
+    }
+
     public override void Interact()
     {
         Debug.Log("I've been interacted with");
---
+++ Spaceship_GGJ_2020/Assets/TorpedoLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class TorpedoLoader : MonoBehaviour
+{
+    [SerializeField] private Transform TheTorpedo = null;
+    [SerializeField] private Transform TheStart = null;
+    [SerializeField] private Transform TheLaunch = null;
+    [Space]
+    [SerializeField] private float slidingTime = 5f;
+
+    private Animator animator;
+    private Action callback;
+    private bool isLoading;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void LoadTorpedo(Action callback)
+    {
+        if (isLoading)
+            return;
+        animator.SetTrigger("Load");
+        this.callback = callback;
+        isLoading = true;
+    }
+
+    public void LoadedTorpedo()
+    {
+        isLoading = false;
+        TheTorpedo.gameObject.SetActive(true);
+        TheTorpedo.position = TheStart.position;
+        TheTorpedo.DOMove(TheLaunch.position, slidingTime);
+    }
+}
